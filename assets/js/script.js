95c89b8df69736a3da98119144859aa21d4307bc
dragon-fire-fly <ek93@hotmail.co.uk>
2022-08-16 11:44:16 +0000

    Add displayQuestion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,37 @@
+/* Question list */
+let question = {
+    id : 1,
+    idiom : "Der Koch war verliebt",
+    literal_translation : "The cook was in love",
+    meaning1 : "To get an extra portion",
+    meaning2 : "The meal is too salty",
+    meaning3 : "To find a hair in your meal",
+    meaning4 : "The meal is already cold",
+    correct_meaning : "The meal is too salty",
+    english_equivalent: ""
+    }
+    
 /* Start game funtion */
 
 /* End game function */
 
 /* Check answer function */
 
-/* Choose question function */
+/* Display question function */
+
+function displayQuestion(q){
+    /* Select the question div from the DOM */
+    let questionContainer = document.getElementById("question-container")
+    /* Modify question div */
+    questionContainer.textContent = q.idiom;
+}
+
+displayQuestion(question)
+
+
+/* Display possible answers */
 
 /* Randomise question function */
+/* (Ensure the question has not already been asked) */
+
+
